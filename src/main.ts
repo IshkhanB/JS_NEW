a6f0b77c4b6afb94bae81847de0649381e6872ba
IshkhanB <b2068666@gmail.com>
2024-07-18 21:18:58 +0300

    1

--- src/main.ts
+++ src/main.ts
@@ -2643,4 +2643,5 @@ import './style.scss'
         new Promise(resolve => setTimeout(() => resolve(2), 2000)), // 2
         new Promise(resolve => setTimeout(() => resolve(3), 1000))  // 3
       ]).then(console.log); // когда все промисы выполнятся, результат будет 1,2,3
+      //https://learn.javascript.ru/event-loop
 }
