ab685efee4418924451cd9c7771580e24b54c48e
IshkhanB <b2068666@gmail.com>
2024-08-01 20:15:10 +0300

    1

--- src/idbm.ts
+++ src/idbm.ts
@@ -1,19 +1,77 @@
 import './idbm.scss'
 
+// const form = document.forms[0]
+// const tileInput = form.children[0] as HTMLInputElement
+
+// const select = form.children[1] as HTMLSelectElement
+
+// form.addEventListener('submit',async (e)=> {
+// e.preventDefault()
+// if(!tileInput.value){
+//   alert('input title')
+//   return
+// }
+
+//     const url1 = `http://www.omdbapi.com/?apikey=383529d9&s=${tileInput.value}&type=${select.value}`
+
+// })
 const form = document.forms[0]
-const tileInput = form.children[0] as HTMLInputElement
-
+const titleInput = form.children[0] as HTMLInputElement
 const select = form.children[1] as HTMLSelectElement
+let data = {} as any
 
-form.addEventListener('submit',async (e)=> {
-e.preventDefault()
-if(!tileInput.value){
-  alert('input title')
-  return
-}
-
-    const url1 = `http://www.omdbapi.com/?apikey=383529d9&s=${tileInput.value}&type=${select.value}`
-    
+form.addEventListener('submit', async (e) => {
+  e.preventDefault()
+  if (!titleInput.value) {
+    alert('Input title')
+    return
+  }
+  // &page=2
+  const url = `http://www.omdbapi.com/?apikey=a821535d&s=${titleInput.value}&type=${select.value}`
+  const resp = await axios(url)
+  // if (data?.Search) {
+  //   data?.Search.push(...resp.data.Search)
+  // } else {
+  data = resp.data
+  // }
+  renderWithPagination(data, 0)
+  // render(resp.data.Search)
 })
 
-function
+const paginationDiv = document.querySelector('#pagination') as HTMLDivElement
+
+paginationDiv.addEventListener('click', (e) => {
+  const target = e.target as HTMLElement
+  if (!target.dataset.page) return false
+  renderWithPagination(data, +target.dataset.page * 3)
+})
+
+function renderWithPagination(data: any, start: number) {
+  render(data.Search?.slice(start, start + 3))
+  const count = Math.ceil(data.Search.length / 3)
+  paginationDiv.innerHTML = ''
+  for (let i = 0; i < count; i++) {
+    paginationDiv.innerHTML += `<button data-page="${i}">${i + 1}</button>`
+  }
+}
+
+const contentDiv = document.querySelector('#content') as HTMLDivElement
+function render(arr: any[]) {
+  contentDiv.innerHTML = ''
+
+  arr?.forEach((el: any) => {
+    const content = `
+    <div>
+      <img src="${el.Poster}" alt="${el.Title}">
+      <div>
+        <p>${el.Type}</p>
+        <b>${el.Title}</b>
+        <p>${el.Year}</p>
+        <button data-id="${el.imdbID}">Details</button>
+      </div>
+    </div>
+    `
+    contentDiv.insertAdjacentHTML('beforeend', content)
+  })
+}
+
